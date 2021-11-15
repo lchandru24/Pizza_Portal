bfc07f459f2bb3a08d99471de6c6fafd57d670e3
lchandru24 <cl@dovercorp.com>
2021-11-15 16:39:36 +0530

    Updated Version

---
+++ SpecFlowProject/Features/SpecFlowFeature3.feature
@@ -0,0 +1,7 @@
+﻿Feature: SpecFlowFeature3
+	Simple calculator for adding two numbers
+
+@mytag
+Scenario: Checking Wrong Pizza Details
+	Given the Pizza Name is FarmHouse
+	Then the PizzaId should not be 8
---
+++ SpecFlowProject/Features/SpecFlowFeature3Steps.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Pizza_Portal.Controllers;
+using Pizza_Portal.Data;
+using Pizza_Portal.Data.Cart;
+using Pizza_Portal.Data.Services;
+using System;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject.Features
+{
+    [Binding]
+    public class SpecFlowFeature3Steps
+    {
+        private readonly IPizzaService pizzaService;
+        private readonly ShoppingCart shoppingCart;
+        private readonly IOrdersService ordersService;
+
+        public AppDbContext context { get; set; }
+        public OrdersController obj1;
+        public PizzaController pizza;
+        public ShoppingCart cart;
+
+        string PizzaName;
+        int id;
+        int expectedId = 8;
+
+        public SpecFlowFeature3Steps()
+        {
+            obj1 = new OrdersController(pizzaService, shoppingCart, ordersService);
+            pizza = new PizzaController(pizzaService, ordersService);
+            cart = new ShoppingCart(context);
+        }
+
+
+        [Given(@"the Pizza Name is FarmHouse")]
+        public void GivenThePizzaNameIsFarmHouse()
+        {
+            PizzaName = "FarmHouse";
+            try
+            {
+                id = context.Pizza.FirstOrDefault(x => x.Name == PizzaName).Id;
+            }
+            catch (Exception e)
+            {
+                id = 9;
+            }
+        }
+        
+        [Then(@"the PizzaId should not be (.*)")]
+        public void ThenThePizzaIdShouldNotBe(int p0)
+        {
+            id.Should().NotBe(p0);
+        }
+    }
+}
